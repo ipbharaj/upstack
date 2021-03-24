b2b51c68f0740f231125962eb1c0713488034671
ipbharaj <bharaj.inderpreet@gmail.com>
2021-03-25 00:01:18 +0530

    Updated Consultation and LabRequest Controllers

--- UPSTAC-Week3-assignment_updated/src/main/java/org/upgrad/upstac/testrequests/consultation/ConsultationController.java
+++ UPSTAC-Week3-assignment_updated/src/main/java/org/upgrad/upstac/testrequests/consultation/ConsultationController.java
@@ -10,10 +10,12 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.upgrad.upstac.config.security.UserLoggedInService;
 import org.upgrad.upstac.exception.AppException;
+import org.upgrad.upstac.testrequests.RequestStatus;
 import org.upgrad.upstac.testrequests.TestRequest;
 import org.upgrad.upstac.testrequests.TestRequestQueryService;
 import org.upgrad.upstac.testrequests.TestRequestUpdateService;
 import org.upgrad.upstac.testrequests.flow.TestRequestFlowService;
+import org.upgrad.upstac.users.User;
 
 import javax.validation.ConstraintViolationException;
 import java.util.List;
@@ -58,8 +60,7 @@ public class ConsultationController {
         //return the result
         // For reference check the method getForTests() method from LabRequestController class
 
-        // replace this line of code with your implementation
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED,"Not implemented");
+        return testRequestQueryService.findBy(RequestStatus.LAB_TEST_COMPLETED);
 
 
     }
@@ -75,11 +76,8 @@ public class ConsultationController {
         //Make use of the findByDoctor() method from testRequestQueryService class to get the list
         // For reference check the method getForTests() method from LabRequestController class
 
-        // replace this line of code with your implementation
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED,"Not implemented");
-
-
-
+        User doctor = userLoggedInService.getLoggedInUser();
+        return testRequestQueryService.findByDoctor(doctor);
 
     }
 
@@ -97,8 +95,9 @@ public class ConsultationController {
         // return the above created object
         // For reference check the method assignForLabTest() method from LabRequestController class
         try {
-            // replace this line of code with your implementation
-            throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED,"Not implemented");
+            User doctor = userLoggedInService.getLoggedInUser();
+            return testRequestUpdateService.assignForConsultation(id,doctor);
+
 
         }catch (AppException e) {
             throw asBadRequest(e.getMessage());
@@ -120,8 +119,10 @@ public class ConsultationController {
         // For reference check the method updateLabTest() method from LabRequestController class
 
         try {
-            // replace this line of code with your implementation
-            throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED,"Not implemented");
+            User doctor = userLoggedInService.getLoggedInUser();
+
+
+            return testRequestUpdateService.updateConsultation(id,testResult,doctor);
 
 
         } catch (ConstraintViolationException e) {
